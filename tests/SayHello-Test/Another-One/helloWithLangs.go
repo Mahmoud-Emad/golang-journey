38d6b473d984e46bd94bae0316883254b9008847
mahmoud emad <mahmoud.emad@sonono.ch>
2022-04-14 22:23:45 +0200

    Adding switchCase and comment out if condation.

--- tests/SayHello-Test/Another-One/helloWithLangs.go
+++ tests/SayHello-Test/Another-One/helloWithLangs.go
@@ -2,17 +2,30 @@ package main
 import "fmt"
 
 
+// Here We'll use if condation
+// func Say(lang, name string) string {
+// 	// This function writes to only say hello to [Name] -> You should pass it when you call it 
+// 	// lang is language("FR", "SP", "EN") -> [Hello, Hola, Oui]
+// 	word := ""
+// 	if lang == "FR" {
+// 		word = "Oui, "
+// 	}else if lang == "SP" { 
+// 		word = "Hola, "
+// 	}else{
+// 		word = "Hello, "
+// 	}
+// 	return word + name
+// }
+// Here We'll use Switch Case
 func Say(lang, name string) string {
 	// This function writes to only say hello to [Name] -> You should pass it when you call it 
 	// lang is language("FR", "SP", "EN") -> [Hello, Hola, Oui]
 	word := ""
-	if lang == "FR" {
-		word = "Oui, "
-	}else if lang == "SP" { 
-		word = "Hola, "
-	}else{
-		word = "Hello, "
-	}
+	switch lang {
+	case "FR":word = "Oui, "
+	case "SP":word = "Hola, "
+	default:word = "Hello, "
+	} 
 	return word + name
 }
 
